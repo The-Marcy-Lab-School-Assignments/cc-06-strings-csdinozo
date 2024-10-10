808922e0f80a05cb062f9f5e05198fc1dcb75274
Catalina Dinozo <csdinozo@gmail.com>
2024-10-10 12:40:23 -0400

    implemented solution using recursion + spaces no longer considered in zig-zag pattern

--- index.js
+++ index.js
@@ -11,11 +11,20 @@ console.log(reverseString("hello"));
 // p2
 const reverseZigZagString = (s) => {
 	let r = reverseString(s);
+	let a = r.split(" ");
 	let z = "";
+	let za = "";
+
+	if (a.length > 1) {
+		for (let i = 0; i < a.length; i++) za += reverseString(reverseZigZagString(a[i])) + " ";
+		return za;
+	}
+
 	for (let i = 0; i < r.length; i++) {
 		if (i % 2 === 0) z += r[i].toLowerCase();
 		else z += r[i].toUpperCase();
 	}
+
 	return z;
 }
 console.log(reverseZigZagString("helloo")) //"oOlLeH"
