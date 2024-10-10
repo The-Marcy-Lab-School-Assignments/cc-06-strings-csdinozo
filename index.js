067df87af0d103522653c29a44ea4097447e8152
Catalina Dinozo <csdinozo@gmail.com>
2024-10-10 10:39:03 -0400

    p1 string reversal using iteration

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //write your code here
+
+// p1
+const reverseString = (s) => {
+	let r = "";
+	for (let i = s.length - 1; i >= 0; i--) r += s[i];
+	return r;
+}
+console.log(reverseString("hello"));
+
